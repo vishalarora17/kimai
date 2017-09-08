e6a5892f35407cfbb8cd5fff0d1b4cd329a15abf
Vishal Arora <vishalgeniuss@gmail.com>
2017-09-08 20:12:52 +0530

    Deleted all files and folder

--- includes/version.php
+++
@@ -1,4 +0,0 @@
-<?php
-$kga['version'] = '1.1.0';
-$kga['revision'] = 1389; // database revision number (incremented whenever the database changes)
-$kga['status'] = ''; // leave blank if stable
--- installer/steps/10_language.php
+++
@@ -1,7 +0,0 @@
-<form id="lang">
-    <div id="lang_sel">
-        <input type="image" class="lang" id="de" value="" onclick="lang_selected('de'); return false;"/>
-        <input type="image" class="lang" id="en" value="" onclick="lang_selected('en'); return false;"/>
-    </div>
-</form>
-<div style="clear:both;"></div>
